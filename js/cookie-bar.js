b3e483893f50425ae3005a4fe8f7783fe3f216a2
JMRoehrig <50141093+JMRoehrig@users.noreply.github.com>
2019-05-02 00:43:42 +1000

    new and updated js files

--- js/cookie-bar.js
+++ js/cookie-bar.js
@@ -1,32 +1,32 @@
 jQuery(document).ready(function($) {
-	if(!euReadCookie("euCookiesAcc")){
-		$("#eu-cookie-bar").show();
+	if(!cookieBarReadCookie("cookieBarCookiesAccept")){
+		$("#cookie-bar-wp").show();
 	}
 });
-function euSetCookie(cookieName, cookieValue, nDays) {
+
+
+function cookieBarSetCookie(cookieName, cookieValue, nDays) {
 	var today = new Date();
 	var expire = new Date();
-	if (nDays==null || nDays==0) nDays=1;
-	expire.setTime(today.getTime() + 3600000*24*nDays);
-	document.cookie = cookieName+"="+escape(cookieValue)+ ";expires="+expire.toGMTString()+"; path=/";
+	if (nDays == null || nDays==0) nDays = 1;
+	expire.setTime(today.getTime() + 3600000 * 24 * nDays);
+	document.cookie = cookieName + "=" + escape(cookieValue) + ";expires=" + expire.toGMTString() + "; path=/";
 }
-function euReadCookie(cookieName) {
-	var theCookie=" "+document.cookie;
-	var ind=theCookie.indexOf(" "+cookieName+"=");
-	if (ind==-1) ind=theCookie.indexOf(";"+cookieName+"=");
-	if (ind==-1 || cookieName=="") return "";
-	var ind1=theCookie.indexOf(";",ind+1);
-	if (ind1==-1) ind1=theCookie.length; 
-	return unescape(theCookie.substring(ind+cookieName.length+2,ind1));
+
+
+function cookieBarReadCookie(cookieName) {
+	var theCookie=" " + document.cookie;
+	var ind = theCookie.indexOf(" " + cookieName + "=");
+	if (ind == -1) ind = theCookie.indexOf(";" + cookieName + "=");
+	if (ind == -1 || cookieName=="") return "";
+	var ind1 = theCookie.indexOf(";", ind + 1);
+	if (ind1 == -1) ind1 = theCookie.length; 
+	return unescape(theCookie.substring(ind + cookieName.length + 2, ind1));
 }
-function euDeleteCookie(cookieName) {
-	var today = new Date();
-	var expire = new Date() - 30;
-	expire.setTime(today.getTime() - 3600000*24*90);
-	document.cookie = cookieName+"="+escape(cookieValue)+ ";expires="+expire.toGMTString();
-}
-function euAcceptCookiesWP() {
-	euSetCookie('euCookiesAcc', true, 365);
-	jQuery("#eu-cookie-bar").hide();
-	jQuery("html").css("margin-top","0");
+
+
+function cookieBarAcceptCookiesWP() {
+	cookieBarSetCookie('cookieBarCookiesAccept', true, 365);
+	jQuery("#cookie-bar-wp").hide();
+	jQuery("html").css("margin-top","0"); /* Why is this line here? Looks like legacy that can be removed. */
 }
