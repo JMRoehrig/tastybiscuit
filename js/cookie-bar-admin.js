fe7b231f2da26f76d5df87234058562d264ecbb2
JMRoehrig <50141093+JMRoehrig@users.noreply.github.com>
2019-05-02 15:22:04 +1000

    Add files via upload

---
+++ js/cookie-bar-admin.js
@@ -0,0 +1,10 @@
+jQuery(document).ready(function($){
+    $(".cookie_bar_background_color").wpColorPicker();
+    $(".cookie_bar_text_color").wpColorPicker();
+    $(".cookie_bar_text_link_color").wpColorPicker();
+    $(".cookie_bar_text_link_hover_color").wpColorPicker();
+    $(".cookie_bar_button_background_color").wpColorPicker();
+    $(".cookie_bar_button_text_color").wpColorPicker();
+    $(".cookie_bar_button_hover_background_color").wpColorPicker();
+    $(".cookie_bar_button_hover_text_color").wpColorPicker();
+});
